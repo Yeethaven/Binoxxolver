3ad95a86bb0dbfc12d603f10285f9b3760a12930
Yeethaven <alex.untch02@gmail.com>
2023-05-09 15:58:03 +0200

    Fixed a bug where game would accept a wrong input

--- game.py
+++ game.py
@@ -1,15 +1,16 @@
 from os import system
+from numpy import copy
 
 from generator import generate, show
-from solver import check
-from checker import check as check_full
+from solver import backtrack
+from checker import check as check_finished
 
 
 def put(grid, r, c, val):
     valNum = 2
     if val == 'X' or val == 'x':
         valNum = 1
-    elif val == 'O' or val == 'o':
+    elif val == 'O' or val == 'o' or val == '0':
         valNum = 0
     else: return False
 
@@ -22,14 +23,21 @@ def put(grid, r, c, val):
 def unput(grid, r, c):
     grid[r][c] = 2
 
+def check_solvable(grid):
+    grid_copy = copy(grid)
+    if backtrack(grid_copy) is None:
+        return False
+    
+    return True
+
 def process_input(input_str):
     # This function processes the input and returns a result
-    input_list = input_str.split(', ')
+    input_list = input_str.split(' ')
     try:
         r = int(input_list[0])
         c = int(input_list[1])
         val = input_list[2]
-    except ValueError:
+    except:
         print("Invalid input!")
         return None
     result = (r-1, c-1, val)
@@ -40,18 +48,17 @@ def game(N, p_empty, lifes):
     grid = generate(N, p_empty)
 
     while not lifes == 0:
-        if(check_full(grid)):
+        if(check_finished(grid)):
             system('clear')
             show(grid)
             print("You won! :)")
             return
         
         system('clear')
-        
         show(grid)
-
         print("\nRemaining lifes: "+str(lifes))
-        input_str = input("\nPlease enter input in the form of 'row, col, val': ")
+
+        input_str = input("\nPlease enter input in the form of 'row col val': ")
         input_processed = process_input(input_str)
         if input_processed is None:
             continue
@@ -60,7 +67,7 @@ def game(N, p_empty, lifes):
         if not put(grid, r, c, val):
             print("Error: One or more illegal inputs!")
         
-        if not check(grid):
+        if check_solvable(grid) is False:
             lifes -= 1
             print("Wrong! Remaining lifes: "+str(lifes))
             unput(grid, r, c)
@@ -71,4 +78,4 @@ def game(N, p_empty, lifes):
 
 
 if __name__ == "__main__":
-    game(8, 0.6, 3)
+    game(8, 0.4, 3)
--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from checker import check
 from solver import backtrack
+from generator import show
 
 def testCheck():
     grid = [[0, 0, 1, 1],
@@ -63,10 +64,22 @@ def testBacktrack():
     
     assert backtrack(grid) == sol
 
+def test_error():
+    grid = [[1, 2, 2, 2, 2, 2, 2, 0],
+            [0, 2, 0, 2, 2, 2, 1, 2],
+            [0, 1, 1, 0, 0, 1, 0, 1],
+            [1, 0, 2, 2, 2, 2, 2, 0],
+            [1, 1, 2, 2, 1, 2, 2, 2],
+            [0, 1, 0, 1, 0, 0, 1, 1],
+            [1, 0, 1, 0, 1, 1, 0, 0],
+            [0, 1, 2, 2, 0, 1, 2, 2]]
+    
+    show(grid)
+    print("check: " + str(check(grid)))
+
 
 def main():
-    testCheck()
-    testBacktrack()
+    test_error();
 
 
 
